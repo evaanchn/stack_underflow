631e2332459be3fa24e6baebffe451700bc5650f
Albin Monge <albin.monge@ucr.ac.cr>
2025-04-05 02:57:45 -0600

    projects/project_01_vessels: Vessels actions control

--- projects/project_01_vessels/src/backend/vessels/Vessel.cpp
+++ projects/project_01_vessels/src/backend/vessels/Vessel.cpp
@@ -10,12 +10,75 @@ Vessel::Vessel(std::string nameInit, int64_t healthInit, size_t costInit
 , weight(weightInit) {
 }
 
+int Vessel::search(int value/*, ActionLog& log*/) {
+  /*
+  log.setResponsible(algorithm.getName());
+  log.setAction("search");
+  log.recordStart();
+  // receives the iterations from algorithm method
+  size_t iterations = algorithm.search(value);
+  log.recordEnd(iterations);
+  return iterations;
+  */
+}
+
+int Vessel::insert(int value/*, ActionLog& log*/) {
+  /*
+  log.setResponsible(algorithm.getName());
+  log.setAction("insert");
+  log.recordStart();
+  // receives the iterations from algorithm method
+  size_t iterations = algorithm.insert(value);
+  log.recordEnd(iterations);
+  return iterations;
+  */
+}
+
+int Vessel::deletion(int value, size_t& upgrade_points/*, ActionLog& log*/) {
+  /*
+  log.setResponsible(algorithm.getName());
+  log.setAction("delete");
+  log.recordStart();
+  // receives the iterations from algorithm method
+  size_t iterations = algorithm.deletion(value, upgrade_points);
+  log.recordEnd(iterations);
+  return iterations;
+  */
+}
+
+void Vessel::fillVessel(/*std::vector<ActionLog>& logs*/) {
+  std::vector<int> numbers;
+  for (int i = 0; i <= ELEMENT_COUNT; ++i) {
+    numbers.push_back(i);
+  }
+  //TODO(any): shuffle numbers
+  /*
+  for (ActionLog log : logs) {
+    if (numbers.empty()) break;
+    this->insert(numbers.back(), log)
+    numbers.pop_back();
+  }
+  */
+}
+
+bool Vessel::isAlive() {
+  return (this->health > 0);
+}
+
 void Vessel::takeDamage(int64_t damage_dealed) {
   if (this->isAlive()) {
     this->health -= damage_dealed;
   }
 }
 
-bool Vessel::isAlive() {
-  return (this->health > 0);
+int Vessel::calculateDamage(/*ActionLog& log*/) {
+  int damage = 0;
+  // create random
+  std::srand(std::time(nullptr));
+  int random_number = (std::rand() % 50) + 1;
+  // search random
+  int deduction = this->search(random_number/*, ActionLog& log*/);
+  // calculate damage
+  damage = MAX_DAMAGE / deduction;
+  return damage;
 }
--- projects/project_01_vessels/src/backend/vessels/Vessel.hpp
+++ projects/project_01_vessels/src/backend/vessels/Vessel.hpp
@@ -2,8 +2,14 @@
 
 #pragma once
 
+#include <cstdlib>
+#include <ctime>
 #include <iostream>
 #include <string>
+#include <vector>
+
+#define ELEMENT_COUNT 50
+#define MAX_DAMAGE (ELEMENT_COUNT * 2)
 
 class Vessel {
  public:
@@ -12,12 +18,41 @@ class Vessel {
       , size_t weightInit);
 
   /**
-   * @brief Decrease the health value of the vessel with a valid amount of
-   * damage
+   * @brief Search given key
    * 
-   * @param damageDealT
+   * @param value key
+   * @param log to register operation time and iterations
+   * @return int iterations taken
    */
-  void takeDamage(int64_t damageDealt);
+  int search(int value/*, ActionLog& log*/);
+
+  /**
+   * @brief Insert given key
+   * 
+   * @param value key
+   * @param log to register operation time and iterations
+   * @return int iterations taken
+   */
+  int insert(int value/*, ActionLog& log*/);  
+  
+   /**
+   * @brief Remove given key
+   * 
+   * @param value key
+   * @param upgrade_points maximum iterations possible
+   * @param log to register operation time and iterations
+   * @return int iterations taken
+   */
+  int Vessel::deletion(int value, size_t& upgrade_points/*, ActionLog& log*/);
+
+  /**
+   * @brief Fill vessel data structure with random values
+   * @details to be called after a vessel is created
+   * 
+   * @param logs to register insetions time and iterations
+   * @return int iterations taken
+   */
+  void fillVessel(/*std::vector<ActionLog>& logs*/);
 
   /**
    * @brief Checks the health to be above 0
@@ -27,7 +62,24 @@ class Vessel {
    */
   bool isAlive();
 
+  /**
+   * @brief Obtain the damage value by doing a search on its data of a
+   * random number
+   * 
+   * @return int damage to be dealt
+   */
+  int calculateDamage();
+
+  /**
+   * @brief Decrease the health value of the vessel with a valid amount of
+   * damage
+   * 
+   * @param damageDealT
+   */
+  void takeDamage(int64_t damageDealt);
+
  protected:
+  /*Algorithm algorithm*/
   std::string name;
   int64_t health = 0;
   size_t cost = 0;
