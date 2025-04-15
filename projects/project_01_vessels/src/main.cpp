0b6b784bf8b19613c07546c102de0f1e59135deb
Albin Monge <albin.monge@ucr.ac.cr>
2025-04-15 12:30:41 -0600

    projects/project_01_vessels: clear main

--- projects/project_01_vessels/src/main.cpp
+++ projects/project_01_vessels/src/main.cpp
@@ -1,14 +1,6 @@
-#include <cstdint>
 #include <iostream>
 #include <SFML/Graphics.hpp>
-#include "backend/algorithms/BTree.hpp"
-#include "backend/algorithms/RedBlackTree.hpp"
 
 int main(void) {
-  BTree* BTree = new BTree(3);
-  for (int64_t i = 0; i < 50; ++i) {
-    BTree->insert(i);
-    printf("Passed\n");
-  }
-  printf("\n");
+  return EXIT_SUCCESS;
 }
