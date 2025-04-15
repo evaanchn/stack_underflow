fff1e5bced0694ed828e9079eadc6caa7b614f7e
Andrey Bejarano <gerardo.bejaranodiaz@ucr.ac.cr>
2025-04-15 17:35:02 -0500

    Game: partially complete

--- projects/project_01_vessels/src/backend/game/Game.cpp
+++ projects/project_01_vessels/src/backend/game/Game.cpp
@@ -1,3 +1,78 @@
 // Copyright 2025 stack_underflow CC-BY 4.0
 
 #include "Game.hpp"
+
+Game::Game() {
+  Player player1;
+  Player player2;
+  players[PLAYER1] = player1;
+  players[PLAYER2] = player2;
+  for (auto player : &players) {
+    player.purchasePoints = 30;
+    player.totalVesselWeight = 15;
+    player.totalVesselWeight = 15;
+  }
+  currentPlayer = 1;
+  winner = 0;
+}
+
+bool Game::canBuyVessel(int64_t vesselID) {
+  const size_t* weights = {5, 3, 2, 2, 2, 1};
+  const size_t* costs = {200, 150, 80, 50, 30, 1};
+  size_t vesselWeight = players[currentPlayer].totalVesselWeight;
+  size_t money = players[currentPlayer].purchasePoints;
+  if (vesselWeight >= weights[vesselID] && money >= costs[vesselID]) {
+    free(weights);
+    free(costs);
+    return true;
+  } else {
+    free(weights);
+    free(costs);
+    return false;
+  }
+}
+
+void Game::consumeAction() {
+  --players[currentPlayer].actions;
+  if (players[currentPlayer].actions == 0) {
+    players[currentPlayer] = 3;
+    if (currentPlayer == PLAYER1) {
+      currentPlayer = PLAYER2;
+    } else {
+      currentPlayer = PLAYER1;
+    }
+  }
+}
+
+int64_t Game::getCurrentPlayer() {
+  return currentPlayer;
+}
+
+bool Game::isSlotOccupied(int64_t row, int64_t col) {
+  // TODO(Andrey): Validade slots
+  // return boad[row][col].isOccupied;
+}
+
+void Game::setVesselAt(int64_t row, int64_t col, int64_t vesselID) {
+  vector<ActionLog> logs;
+  std::vector<int64_t> coodinates;
+  coodinates.push_back(row);
+  coodinates.push_back(col);
+  if (insertVessel(coordinates, vesselID, currentPlayer, &logs)) {
+    printf("Vessel inserted succesfully");
+  } else {
+    printf("Error: couldn't insert the vessel");
+  }
+  for (auto log : &logs) {
+    recordAction(log);
+  }
+}
+
+bool Game::validadeVesselWeight() {
+  size_t vesselWeight = players[currentPlayer].totalVesselWeight;
+  if (vesselWeight == 0 || vesselWeight == MAX_VESSEL_WEIGHT) {
+    return false;
+  } else {
+    return true;
+  }
+}
--- projects/project_01_vessels/src/backend/game/Game.hpp
+++ projects/project_01_vessels/src/backend/game/Game.hpp
@@ -4,51 +4,73 @@
 
 #include <cstddef>
 #include <cstdint>
+#include <map>
+#include <vector>
+
+#include "Board.hpp"
+#include "BattleLog.hpp"
+#inclide "ActionLog.hpp";
 
 // player IDs
 #define PLAYER1 1
 #define PLAYER2 2
+#define MAX_VESSEL_WEIGHT 10
 
 struct Player {
   size_t purchasePoints;
   size_t upgradePoints;
   size_t totalVesselWeight;
+  size_t actions;
 };
 
 class Game {
  public:
   /**
-   * @brief pendiente ...
-   * @return true or false
-   */
-  bool validadeVesselWeight();
-
-  /**
-   * @brief pendiente ...
-   * @return
-   */
-  int64_t getCurrentPlayer();
-
-  /**
    * @brief pendiente
+   * @return true if current player can buy specified vessel based on vessels 
+     weight and money
    */
-  void setVesselAt(int64_t, int64_t col, int64_t vesselType);
+  bool canBuyVessel(int64_t vesselID);
 
   /**
-   * @brief pendiente
+   * @brief validades if a player has more actions in a round
+   */
+  void consumeAction();
+
+  /**
+   * @brief report who is the player that has the turn in the round
+   * @return current player
+   */
+  size_t getCurrentPlayer();
+
+  /// @brief constructor method of the class
+  Game();
+
+  /**
+   * @brief checks out if a slot in the board is available
    * @return true or false
    */
   bool isSlotOccupied(int64_t row, int64_t col);
 
   /**
-   * @brief pendiente
-   * @return true if current player can buy specified vessel based on vessels 
-     weight and money
+   * @brief saves a vessels in the specified coordinates
+   * @param row selected row
+   * @param row selected column
+   * @param vesselID kind of vessel for being saved
    */
-  bool canBuyVessel(int64_t vesselType);
+  void setVesselAt(int64_t row, int64_t col, int64_t vesselID);
+
+  /**
+   * @brief validades if a player has the capacity to save more vessels
+   * @return true or false
+   */
+  bool validadeVesselWeight();
 
  private:
+  map<size_t, Player> players;
   bool gameOver;
   int64_t winner;
-  int64_t currentPlayer;
+  size_t currentPlayer;
+  BattleLog battlelog;
+  Board board;
 };
