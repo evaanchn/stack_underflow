18b81db822d369b6ad60d9f62a7c8ed82c130a58
gerardo.bejaranodiaz <gerardo.bejaranodiaz@ucr.ac.cr>
2025-05-28 00:39:32 -0500

    saved changes

--- projects/project_02_conquest/src/backend/algorithms/GreedySearch.hpp
+++ projects/project_02_conquest/src/backend/algorithms/GreedySearch.hpp
@@ -2,11 +2,13 @@
 
 #pragma once
 
-#include <unordered_map>
 #include <queue>
+#include <unordered_map>
 #include <unordered_set>
 
 #include "Algorithm.hpp"
+#include "Graph.hpp"
+#include "Node.hpp"
 
 /**
  * @brief Greedy algorithm over a graph
@@ -15,8 +17,28 @@ template <typename DataType, typename WeightType>
 class GreedySearch : public Algorithm<DataType, WeightType> {
  public:
   /// @brief Constructor of the class
-  GreedySearch();
+  GreedySearch() {
+    this->algorithmName = "Greedy";
+  }
+
   /// Destructor of the class
-  ~GreedySearch();
- private:
+  ~GreedySearch() {}
+
+  /**
+   * @brief
+   */
+  int64_t explore();
+
+  /**
+   * @brief Greedy search algorithm
+   * @param startingNode first element to visit
+   * @param adjacencyList read only map with available edges from each node
+   * @param limit max number of elements to visit
+   * @return std::unordered_set<DataType> visited container, no order
+   */
+  std::unordered_set<DataType> breadthFirstSearch(Node<DataType>* startingNode
+    , const std::unordered_map<Node<DataType>*
+    , std::unordered_map<Node<DataType>*, WeightType>>& adjacencyList
+    , const size_t limit = SIZE_MAX) {
+  }
 };
