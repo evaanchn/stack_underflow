01ead979ef2621281349e6bf5763e6a09452687d
gerardo.bejaranodiaz <gerardo.bejaranodiaz@ucr.ac.cr>
2025-05-29 12:31:33 -0500

    algorithm: Greedy Search and Attack Algorithm template

---
+++ projects/project_02_conquest/src/backend/algorithms/AttackAlgorithm.hpp
@@ -0,0 +1,34 @@
+// Copyright 2025 stack_underflow CC-BY 4.0
+
+#pragma once
+
+#include <unordered_map>
+#include <queue>
+#include <unordered_set>
+
+#include "Algorithm.hpp"
+#include "Graph.hpp"
+#include "Node.hpp"
+
+/**
+ * @brief Breadth first search algorithm over a graph
+ */
+template <typename DataType, typename WeightType>
+class AttackAlgorithm : public Algorithm<DataType, WeightType> {
+ public:
+  /// Constructor
+  AttackAlgorithm() = default;
+
+  /**
+   * @brief Attack type algorithm
+   * 
+   * @param startingNode first element to visit
+   * @param adjacencyList read only map with available edges from each node
+   * @param totalWeight total weight of the attack
+   * @return size_t iterations taken
+   */
+  virtual size_t attack(
+      Node<DataType>* startingNode, const std::unordered_map<Node<DataType>*
+        , std::unordered_map<Node<DataType>*, WeightType>>& adjacencyList,
+      size_t& totalWeight) = 0;
+};
--- projects/project_02_conquest/src/backend/algorithms/GreedySearch.hpp
+++ projects/project_02_conquest/src/backend/algorithms/GreedySearch.hpp
@@ -6,7 +6,7 @@
 #include <unordered_map>
 #include <unordered_set>
 
-#include "Algorithm.hpp"
+#include "AttackAlgorithm.hpp"
 #include "Graph.hpp"
 #include "Node.hpp"
 
@@ -14,31 +14,57 @@
  * @brief Greedy algorithm over a graph
  */
 template <typename DataType, typename WeightType>
-class GreedySearch : public Algorithm<DataType, WeightType> {
+class GreedySearch : public AttackAlgorithm<DataType, WeightType> {
  public:
   /// @brief Constructor of the class
   GreedySearch() {
     this->algorithmName = "Greedy";
   }
 
-  /// Destructor of the class
-  ~GreedySearch() {}
-
   /**
-   * @brief
+   * @brief Greedy Search: always follows the locally cheapest edge
+   *        until reaching a previously explored node
+   *
+   * @param startNode Initial node
+   * @param adjacencyList Adjacency map with edge weights
+   * @param totalWeight Total weight accumulated until reaching a visited node
+   * @return size_t Iterations (number of edges considered)
    */
-  int64_t explore();
-
-  /**
-   * @brief Greedy search algorithm
-   * @param startingNode first element to visit
-   * @param adjacencyList read only map with available edges from each node
-   * @param limit max number of elements to visit
-   * @return std::unordered_set<DataType> visited container, no order
-   */
-  std::unordered_set<DataType> breadthFirstSearch(Node<DataType>* startingNode
-    , const std::unordered_map<Node<DataType>*
-    , std::unordered_map<Node<DataType>*, WeightType>>& adjacencyList
-    , const size_t limit = SIZE_MAX) {
+  size_t attack(Node<DataType>* startNode, Node<DataType>* endNode
+      , const std::unordered_map<Node<DataType>*
+        , std::unordered_map<Node<DataType>*, WeightType>>& adjacencyList
+      , size_t& totalWeight) override {
+    // iterations taken by the algorithm
+    size_t iterations = 0;
+    // set used for saving visited nodes
+    std::unordered_set<DataType> visited;
+    // starts with startNode
+    Node<DataType>* current = startNode;
+    visited.insert(current->getData());
+    while (current != goalNode) {
+      // gets all the neighbors of the current node
+      const auto& neighbors = adjacencyList.at(current);
+      Node<DataType>* nextNode = nullptr;
+      // Variable to keep track of the minimum weight
+      WeightType minWeight = std::numeric_limits<WeightType>::max();
+      // Pick the cheapest edge to an unvisited neighbor
+      for (const auto& [neighbor, weight] : neighbors) {
+        ++iterations;
+        // Explore all the neigbors to get the cheapest one
+        if (visited.count(neighbor->getData()) == 0 && weight < minWeight) {
+          minWeight = weight;
+          nextNode = neighbor;
+        }
+      }
+      if (nextNode == nullptr) {
+        // Dead-end reached: no unvisited neighbors
+        break;
+      }
+      // Move along that edge
+      totalWeight += minWeight;
+      current = nextNode;
+      visited.insert(current->getData());
+    }
+    return iterations;
   }
 };
