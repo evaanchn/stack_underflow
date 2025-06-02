fd9c4386b29cb3cb27961adc37af116f34ef830c
Evan Chen <evan.chen@ucr.ac.cr>
2025-06-01 22:23:49 -0600

    galaxy: Added initial implementation of galaxy

---
+++ projects/project_02_conquest/src/backend/galaxy/Galaxy.cpp
@@ -0,0 +1,39 @@
+// Copyright 2025 stack_underflow CC-BY 4.0
+
+#include "Galaxy.hpp"
+
+Galaxy::Galaxy(const char* galaxyFileDirectory) {
+  this->setUpGalaxy(galaxyFileDirectory);
+}
+
+Galaxy::~Galaxy() {
+  this->destroyCurrentSolarSystem();
+}
+
+void Galaxy::setUpGalaxy(const char* galaxyFileDirectory) {
+  // TODO (any) add try catch for galaxy instancing
+  CSVReader reader = CSVReader();
+  reader.decomposeLines(galaxyFileDirectory, this->solarSystemsData);
+}
+
+void Galaxy::createCurrentSolarSystem() {
+  std::vector<std::string>& currentSolarSystemData
+      = this->solarSystemsData[this->currentSolarSystemIndex];
+  try {
+    SolarSystemVerifier().analyzeSolarSystemData(currentSolarSystemData);
+    this->currentSolarSystem = new SolarSystem(currentSolarSystemData);
+  } catch (const SolarSystemException& e) {
+    std::cerr << "Solar System error with #" << this->currentSolarSystemIndex
+        << ": " << e.what() << std::endl;
+    ++this->currentSolarSystemIndex;
+  }
+}
+
+void Galaxy::destroyCurrentSolarSystem() {
+  if (this->currentSolarSystem) delete this->currentSolarSystem;
+  this->currentSolarSystem = nullptr;
+}
+
+SolarSystem* Galaxy::getCurrentSolarSystem() {
+  return this->currentSolarSystem;
+}
