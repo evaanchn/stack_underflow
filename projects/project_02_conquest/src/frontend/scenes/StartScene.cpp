611b3c9e1a38f79539ede9eefacb7b5841d41beb
gerardo.bejaranodiaz <gerardo.bejaranodiaz@ucr.ac.cr>
2025-06-03 01:39:05 -0500

    scenes: StartScene initialization

---
+++ projects/project_02_conquest/src/frontend/scenes/StartScene.cpp
@@ -0,0 +1,13 @@
+// Copyright 2025 stack_underflow CC-BY 4.0
+
+#include "StartScene.hpp"
+
+StartScene::StartScene() {
+}
+
+void StartScene::handleEvent(sf::RenderWindow& window, sf::Event& event,
+    SceneState& currentState) {
+}
+
+void StartScene::draw(sf::RenderWindow& window) {
+}
---
+++ projects/project_02_conquest/src/frontend/scenes/StartScene.hpp
@@ -0,0 +1,20 @@
+// Copyright 2025 stack_underflow CC-BY 4.0
+
+#pragma once
+
+#include "Button.hpp"
+#include "SFMLImage.hpp"
+#include "SFMLSound.hpp"
+
+class StartScene {
+ public:
+  StartScene();
+  void handleEvent(sf::RenderWindow& window, sf::Event& event,
+      SceneState& currentState);
+  void draw(sf::RenderWindow& window);
+
+ private:
+  SFMLImage background;
+  Button startButton;
+  Button infoButton;
+};
