afa799e719ba6d04460ba1081559b8be67230fd3
Evan Chen <evan.chen@ucr.ac.cr>
2025-06-04 00:51:30 -0600

    game_elements: Completed UIPlanets class with random appearances

---
+++ projects/project_02_conquest/src/frontend/game_elements/UIPlanet.hpp
@@ -0,0 +1,73 @@
+// Copyright 2025 stack_underflow CC-BY 4.0
+
+#pragma once
+
+#include <FL/Fl_PNG_Image.H>
+#include <string>
+
+#include "LayeredButton.hpp"
+
+#include "Random.hpp"
+
+enum PLANET_LAYERS {
+  SPHERE, TERRAIN, OCCUPIER
+};
+
+#define AVAILABLE_SPHERE_SPRITES 10
+#define AVAILABLE_TERRAIN_SPRITES 10
+
+const std::string SPRITES_PATH = "assets/sprites/";
+const std::string SPHERE_PATH = SPRITES_PATH + "planet/sphere/";
+const std::string TERRAIN_PATH = SPRITES_PATH + "planet/terrain/";
+const std::string SPHERE_SPRITE = "planet";
+const std::string TERRAIN_SPRITE = "planet";
+const std::string SPRITES_EXTENSION = ".png";
+
+/// @class UIPlanet
+/// @brief Represents a planet using a layered FLTK button.
+class UIPlanet {
+ private:
+  /// @brief The underlying FLTK layered button used to render the planet.
+  LayeredButton* button = nullptr;
+
+ public:
+  /// @brief Default constructor. Does not initialize the planet button.
+  UIPlanet() = default;
+
+  /// @brief Constructs a UIPlanet at a given position with specified size.
+  /// @param x The x-coordinate of the planet's position.
+  /// @param y The y-coordinate of the planet's position.
+  /// @param dimension The width and height of the square-shaped planet button.
+  explicit UIPlanet(size_t x, size_t y, size_t dimension);
+
+  /// @brief Destructor. Cleans up dynamically allocated resources.
+  ~UIPlanet();
+
+ private:
+  /// @brief Sets the appearance of the planet using sphere and terrain sprites.
+  void setPlanetAppearance();
+
+  /// @brief Constructs a file path to a random sprite from available.
+  /// @param initialLayerPath The base path for the sprite layer.
+  /// @param availableSprites The total number of available sprite variants.
+  /// @return The complete path to a randomly selected sprite.
+  std::string constructLayerPath(const std::string& initialLayerPath
+      , int availableSprites);
+
+ public:
+  /// @brief Makes the planet visible and interactive in the UI.
+  void reveal();
+
+  /// @brief Sets an image layer to represent an occupier (e.g., unit or object)
+  /// on the planet.
+  /// @param occupier A pointer to a `Fl_PNG_Image` to be used as the occupier's
+  /// image.
+  void setOccupier(Fl_PNG_Image* occupier);
+
+  /// @brief Removes the occupier image layer from the planet.
+  void removeOccupier();
+
+  /// @brief Assigns a function to execute when the planet is clicked.
+  /// @param func The action the planet's button must carry out
+  void setPlanetInteraction(std::function<void()> func);
+};
